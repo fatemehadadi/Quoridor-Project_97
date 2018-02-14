384d376bf8644887bf12c87455309612cb3c100b
Fateme <tatianatehran@gmail.com>
2018-02-14 06:17:50 +0000

    Upload New File

---
+++ quoridor_client/web_urls.py
@@ -0,0 +1,16 @@
+# here is the path of datas given to webserver
+urls = {
+
+    'signup': 'http://127.0.0.1:8000/signup/',
+
+    'signin': 'http://127.0.0.1:8000/signup/',
+
+    'quoridor2': 'http://127.0.0.1:8000/quoridor2/',
+
+    'quoridor4': 'http://127.0.0.1:8000/quoridor4/',
+
+    'signout': 'http://127.0.0.1:8000/signout/',
+
+
+
+}
